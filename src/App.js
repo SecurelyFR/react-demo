9b841bd47d61147eea4b03fb80c4ee249d330bd0
Jean-Christophe Rona <jc@rona.fr>
2024-07-17 17:06:01 +0200

    @securely/websdk example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { showPolicy } from '@securely.id/websdk';
 
-function App() {
+const App = () => {
+  const [policy, setPolicy] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPolicy = async () => {
+      try {
+        const chainId = 11155111;
+        const dappAddr = '0xddd576a49d827b1e6f2a7fe7969e1cb4e2af7d6f';
+        const functionSelector = '0x666c6f23';
+        const formatted = true;
+
+        const result = await showPolicy(chainId, dappAddr, functionSelector, formatted);
+        setPolicy(result);
+      } catch (error) {
+        setError(`Error: ${error.message}`);
+      }
+    };
+
+    fetchPolicy();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Securely ID Show Policy</h1>
+        {error ? (
+          <p>{error}</p>
+        ) : policy ? (
+          <div dangerouslySetInnerHTML={{__html: policy.result}} />
+        ) : (
+          <p>Loading policy...</p>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
